a929534534a5cf4fe9ceda935e46031ce4c7c5b6
keylat160 <keylat160@gmail.com>
2024-01-04 22:44:07 -0800

    final commit

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,106 +1,71 @@
+let colorName = document.getElementById('color-name');
+let pricePerGallon = document.getElementById('color-price');
+
+
 function configureListeners() {
-   const images = document.getElementsById("pn1");
-   const images = document.getElementsById("pn2");
-   const images = document.getElementsById("pn3");
-   const images = document.getElementsById("pn4");
-   const images = document.getElementsById("pn5");
-   const images = document.getElementsById("pn6");
-   const images = document.getElementsById("pn7");
-   const images = document.getElementsById("pn8");
-   const images = document.getElementsById("pn9");
+  let images = document.querySelectorAll('img')
+
+    for (let i = 0; i < images.length; i++) {          
+        images[i].addEventListener('mouseover', (event) => {
+            addOpacity(event)
+        })
+        images[i].addEventListener('mouseout', (event) => {
+            removeOpacity(event)
+
+        })
+    }
 }
-
-    // for (let i = 0; i < images.length; i++) {          
-        //images[i].addEventListener('mouseover', addOpacity, false);
-        //images[i].addEventListener('mouseout', removeOpacity, false);
-        images.forEach(function(image) {
-            image.addEventListener('mouseover', addOpacity, false);
-            image.addEventListener('mouseout', removeOpacity, false);
-        });
-
+     
 function addOpacity(event) {
-    if (!this.classList.contains('dim')){
-        this.classList.add('dim')
-    }   // add appropriate CSS class
+    event.target.classList.toggle('dim')
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
-     //remove appropriate CSS class
-     if (this.classList.contains('dim')){
-        this.classList.remove('dim');
-    }
-
-    let element = document.getElementById('color-price');
-        element.textContent = '';
-        
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
-
-    event.preventDefault();    
+event.target.classList.toggle('dim')
+price.textContent = '';
+color.textContent = '';
+event.preventDefault();    
 }
 
 function getProductInfo(paintColor) {
-    let price;
-    let colorName;  
+      
     
     switch (paintColor) {
     case 'pn1':           
-            price: = '$14.99';
-            colorName: = 'Lime Green';
-            updatePrice(colorName, price) 
+            updatePrice('Lime Green', '$14.99') 
         break;           
     case 'pn2':
-                price: = '$11.14'; 
-                colorName: = 'Medium Brown';
-                updatePrice(colorName, price) 
+                updatePrice('Medium Brown', '$11.14') 
         break;            
     case 'pn3':
-                price: = '$22.99';
-                colorName: = 'Royal Blue';
-                updatePrice(colorName, price)  
+                updatePrice('Royal Blue', '$22.99')  
         break;   
     case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
-                price: = '$4.99';
-                colorName: = 'Solid Black';
-                updatePrice(colorName, price) 
+                updatePrice('Solid Black', '$4.99') 
         break;   
     case 'pn5':
-                price: = '$8.22';
-                colorName: = 'Solid Cyan';
-                updatePrice(colorName, price) 
+                updatePrice('Solid Cyan', '$8.22') 
         break;   
     case 'pn6':
-                price: = '$11.99';
-                colorName: = 'Solid Purple';
-                updatePrice(colorName, price) 
+                updatePrice('Solid Purple', '$11.99') 
         break;   
      case 'pn7':
-                price: = '$13.42'; 
-                colorName: = 'Solid Red';
-                updatePrice(colorName, price) 
+                updatePrice('Solid Red', '$13.42') 
         break;
     case 'pn8':
-                price: = '$21.98'; 
-                colorName: = 'Solid White';
-                updatePrice(colorName, price) 
+                updatePrice('Solid White', '$21.98') 
         break;   
      case 'pn9':
-                price: = '$14.99';
-                colorName: = 'Solid Yellow';
-                updatePrice(colorName, price) 
+                updatePrice('Solid Yellow', '$14.99') 
         break;   
       default:              
     }
 
-    function updatePrice(colorName, price)
-    {       
-        let colorPrice = document.getElementById("color-price"); // select element with corresponding id
-        colorPrice.textContent = price;// display price
-        
-        let color = document.getElementById("color-name");// select element with corresponding id
-        color.textContent = colorName;//display color name
+    function updatePrice(color, price)
+    {  
+        colorName.textContent = color;
+        pricePerGallon.textContent = price;
     }
-    
 }
+    
