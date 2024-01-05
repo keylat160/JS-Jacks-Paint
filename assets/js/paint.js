8fe6c0ae7b0ecc740eb29e42d7bedf679d1e994f
keylat160 <keylat160@gmail.com>
2024-01-04 22:53:13 -0800

    final commit2

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -42,19 +42,19 @@ function getProductInfo(paintColor) {
                 updatePrice('Royal Blue', '$22.99')  
         break;   
     case 'pn4':
-                updatePrice('Solid Black', '$4.99') 
+                updatePrice('Solid Red', '$4.99') 
         break;   
     case 'pn5':
-                updatePrice('Solid Cyan', '$8.22') 
+                updatePrice('Solid white', '$8.22') 
         break;   
     case 'pn6':
-                updatePrice('Solid Purple', '$11.99') 
+                updatePrice('Solid Black', '$11.99') 
         break;   
      case 'pn7':
-                updatePrice('Solid Red', '$13.42') 
+                updatePrice('Solid Cyan', '$13.42') 
         break;
     case 'pn8':
-                updatePrice('Solid White', '$21.98') 
+                updatePrice('Solid Purple', '$21.98') 
         break;   
      case 'pn9':
                 updatePrice('Solid Yellow', '$14.99') 
